b54f607701a1c0f5152a7b430999022b3c3c5ad4
PrismoFinance <moleculardot@proton.me>
2024-09-16 21:28:09 -0400

    Update destination.rs

--- contracts/dca/src/types/destination.rs
+++ contracts/dca/src/types/destination.rs
@@ -1,6 +1,8 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Binary, Decimal};
 
+
+// Where funds should be sent once escrow verification. 
 #[cw_serde]
 pub struct Destination {
     pub allocation: Decimal,
