814b4d3ef58e2ea7322ee1366969ed5ad9b3faa4
PrismoFinance <moleculardot@proton.me>
2024-09-17 13:24:06 -0400

    Update get_vault_performance.rs

--- contracts/dca/src/handlers/get_vault_performance.rs
+++ contracts/dca/src/handlers/get_vault_performance.rs
@@ -1,55 +1,55 @@
 use crate::{
     helpers::{fees::get_performance_fee, price::get_twap_to_now, vault::get_performance_factor},
-    msg::VaultPerformanceResponse,
-    state::{config::get_config, vaults::get_vault},
+    msg::BountyPerformanceResponse,
+    state::{config::get_config, bounties::get_bounty},
 };
 use cosmwasm_std::{Deps, StdError, StdResult, Uint128};
 
-pub fn get_vault_performance_handler(
+pub fn get_bounty_performance_handler(
     deps: Deps,
-    vault_id: Uint128,
-) -> StdResult<VaultPerformanceResponse> {
-    let vault = get_vault(deps.storage, vault_id)?;
+    bounty_id: Uint128,
+) -> StdResult<BountyPerformanceResponse> {
+    let bounty = get_bounty(deps.storage, bounty_id)?;
 
     let config = get_config(deps.storage)?;
 
     let current_price = get_twap_to_now(
         &deps.querier,
         config.exchange_contract_address.clone(),
-        vault.get_swap_denom(),
-        vault.target_denom.clone(),
+        bounty.get_swap_denom(),
+        bounty.target_denom.clone(),
         config.twap_period,
-        vault.route.clone(),
+        bounty.route.clone(),
     )?;
 
-    vault.performance_assessment_strategy.clone().map_or(
+    bounty.performance_assessment_strategy.clone().map_or(
         Err(StdError::GenericErr {
             msg: format!(
-                "Vault {} does not have a performance assessment strategy",
-                vault_id
+                "Bounty {} does not have a performance assessment strategy",
+                bounty_id
             ),
         }),
         |_| {
-            Ok(VaultPerformanceResponse {
-                fee: get_performance_fee(&vault, current_price)?,
-                factor: get_performance_factor(&vault, current_price)?,
+            Ok(BountyPerformanceResponse {
+                fee: get_performance_fee(&bounty, current_price)?,
+                factor: get_performance_factor(&bounty, current_price)?,
             })
         },
     )
 }
 
 #[cfg(test)]
-mod get_vault_performance_tests {
-    use super::get_vault_performance_handler;
+mod get_bounty_performance_tests {
+    use super::get_bounty_performance_handler;
     use crate::{
         constants::{ONE, TEN},
         tests::{
-            helpers::{instantiate_contract, setup_vault},
+            helpers::{instantiate_contract, setup_bounty},
             mocks::{calc_mock_dependencies, ADMIN, DENOM_UKUJI, DENOM_UUSK},
         },
         types::{
             performance_assessment_strategy::PerformanceAssessmentStrategy,
-            swap_adjustment_strategy::SwapAdjustmentStrategy, vault::Vault,
+            swap_adjustment_strategy::SwapAdjustmentStrategy, bounty::Bounty,
         },
     };
     use cosmwasm_std::{
@@ -58,19 +58,19 @@ mod get_vault_performance_tests {
     };
 
     #[test]
-    fn if_vault_has_no_performance_assessment_strategy_fails() {
+    fn if_bounty_has_no_performance_assessment_strategy_fails() {
         let mut deps = calc_mock_dependencies();
         let env = mock_env();
 
         instantiate_contract(deps.as_mut(), env.clone(), mock_info(ADMIN, &[]));
 
-        let vault = setup_vault(deps.as_mut(), env, Vault::default());
+        let bounty = setup_bounty(deps.as_mut(), env, Bounty::default());
 
-        let err = get_vault_performance_handler(deps.as_ref(), vault.id).unwrap_err();
+        let err = get_bounty_performance_handler(deps.as_ref(), bounty.id).unwrap_err();
 
         assert_eq!(
             err.to_string(),
-            "Generic error: Vault 0 does not have a performance assessment strategy"
+            "Generic error: Bounty 0 does not have a performance assessment strategy"
         );
     }
 
@@ -88,21 +88,21 @@ mod get_vault_performance_tests {
             received_amount: Coin::new(standard_received_amount.into(), DENOM_UUSK),
         };
 
-        let vault = setup_vault(
+        let bounty = setup_bounty(
             deps.as_mut(),
             env,
-            Vault {
+            Bounty {
                 swapped_amount: Coin::new(TEN.into(), DENOM_UUSK),
                 received_amount: Coin::new(TEN.into(), DENOM_UUSK),
                 escrowed_amount: Coin::new(TEN.into(), DENOM_UUSK),
                 swap_adjustment_strategy: Some(SwapAdjustmentStrategy::default()),
                 performance_assessment_strategy: Some(performance_assessment_strategy),
                 escrow_level: Decimal::percent(5),
-                ..Vault::default()
+                ..Bounty::default()
             },
         );
 
-        let response = get_vault_performance_handler(deps.as_ref(), vault.id).unwrap();
+        let response = get_bounty_performance_handler(deps.as_ref(), bounty.id).unwrap();
 
         assert_eq!(
             response.fee,
