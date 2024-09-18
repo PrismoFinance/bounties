86085f3381b28f9368b2a0a54f7844f72789f515
PrismoFinance <moleculardot@proton.me>
2024-09-18 09:19:16 -0400

    Update contract.rs

--- contracts/dca/src/contract.rs
+++ contracts/dca/src/contract.rs
@@ -3,8 +3,8 @@ use crate::constants::{
     AFTER_SWAP_REPLY_ID, FAIL_SILENTLY_REPLY_ID,
 };
 use crate::error::ContractError;
-use crate::handlers::cancel_vault::cancel_vault_handler;
-use crate::handlers::create_vault::{create_vault_handler, save_price_trigger};
+use crate::handlers::cancel_vault::cancel_bounty_handler;
+use crate::handlers::create_vault::{create_bounty_handler, save_price_trigger};
 use crate::handlers::deposit::deposit_handler;
 use crate::handlers::disburse_escrow::disburse_escrow_handler;
 use crate::handlers::disburse_funds::disburse_funds_handler;
@@ -16,16 +16,16 @@ use crate::handlers::get_events_by_resource_id::get_events_by_resource_id_handle
 use crate::handlers::get_pairs::get_pairs_handler;
 use crate::handlers::get_time_trigger_ids::get_time_trigger_ids_handler;
 use crate::handlers::get_trigger_id_by_fin_limit_order_idx::get_trigger_id_by_fin_limit_order_idx_handler;
-use crate::handlers::get_vault::get_vault_handler;
-use crate::handlers::get_vault_performance::get_vault_performance_handler;
-use crate::handlers::get_vaults::get_vaults_handler;
-use crate::handlers::get_vaults_by_address::get_vaults_by_address_handler;
+use crate::handlers::get_vault::get_bounty_handler;
+use crate::handlers::get_vault_performance::get_bounty_performance_handler;
+use crate::handlers::get_vaults::get_bounties_handler;
+use crate::handlers::get_vaults_by_address::get_bounties_by_address_handler;
 use crate::handlers::handle_failed_automation::handle_failed_automation_handler;
 use crate::handlers::instantiate::instantiate_handler;
 use crate::handlers::migrate::migrate_handler;
 use crate::handlers::update_config::update_config_handler;
 use crate::handlers::update_swap_adjustment_handler::update_swap_adjustment_handler;
-use crate::handlers::update_vault::update_vault_handler;
+use crate::handlers::update_vault::update_bounty_handler;
 use crate::handlers::z_delegate::{log_delegation_result, z_delegate_handler};
 use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 use cosmwasm_std::from_json;
@@ -62,7 +62,7 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::CreateVault {
+        ExecuteMsg::CreateBounty {
             owner,
             label,
             destinations,
@@ -76,7 +76,7 @@ pub fn execute(
             target_receive_amount,
             performance_assessment_strategy,
             swap_adjustment_strategy,
-        } => create_vault_handler(
+        } => create_bounty_handler(
             deps,
             env,
             &info,
@@ -94,7 +94,7 @@ pub fn execute(
             performance_assessment_strategy,
             swap_adjustment_strategy,
         ),
-        ExecuteMsg::UpdateVault {
+        ExecuteMsg::UpdateBounty {
             vault_id,
             label,
             destinations,
@@ -103,7 +103,7 @@ pub fn execute(
             time_interval,
             swap_adjustment_strategy,
             swap_amount,
-        } => update_vault_handler(
+        } => update_bounty_handler(
             deps,
             env,
             info,
@@ -116,12 +116,12 @@ pub fn execute(
             swap_adjustment_strategy,
             swap_amount,
         ),
-        ExecuteMsg::CancelVault { vault_id } => cancel_vault_handler(deps, env, info, vault_id),
+        ExecuteMsg::CancelBounty { bounty_id } => cancel_bounty_handler(deps, env, info, bounty_id),
         ExecuteMsg::ExecuteTrigger { trigger_id, route } => {
             execute_trigger_handler(deps, env, trigger_id, route)
         }
-        ExecuteMsg::Deposit { address, vault_id } => {
-            deposit_handler(deps, env, info, address, vault_id)
+        ExecuteMsg::Deposit { address, bounty_id } => {
+            deposit_handler(deps, env, info, address, bounty_id)
         }
         ExecuteMsg::UpdateConfig {
             executors,
@@ -207,24 +207,24 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
         QueryMsg::GetTriggerIdByFinLimitOrderIdx { order_idx } => to_json_binary(
             &get_trigger_id_by_fin_limit_order_idx_handler(deps, order_idx)?,
         ),
-        QueryMsg::GetVaults {
+        QueryMsg::GetBounties {
             start_after,
             limit,
             reverse,
-        } => to_json_binary(&get_vaults_handler(deps, start_after, limit, reverse)?),
-        QueryMsg::GetVaultsByAddress {
+        } => to_json_binary(&get_bounties_handler(deps, start_after, limit, reverse)?),
+        QueryMsg::GetBountiesByAddress {
             address,
             status,
             start_after,
             limit,
-        } => to_json_binary(&get_vaults_by_address_handler(
+        } => to_json_binary(&get_bounties_by_address_handler(
             deps,
             address,
             status,
             start_after,
             limit,
         )?),
-        QueryMsg::GetVault { vault_id } => to_json_binary(&get_vault_handler(deps, vault_id)?),
+        QueryMsg::GetBounty { bounty_id } => to_json_binary(&get_bounty_handler(deps, bounty_id)?),
         QueryMsg::GetEventsByResourceId {
             resource_id,
             start_after,
@@ -244,7 +244,7 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
         } => to_json_binary(&get_events_handler(deps, start_after, limit, reverse)?),
         QueryMsg::GetConfig {} => to_json_binary(&get_config_handler(deps)?),
         QueryMsg::GetVaultPerformance { vault_id } => {
-            to_json_binary(&get_vault_performance_handler(deps, vault_id)?)
+            to_json_binary(&get_bounty_performance_handler(deps, bounty_id)?)
         }
         QueryMsg::GetDisburseEscrowTasks { limit } => {
             to_json_binary(&get_disburse_escrow_tasks_handler(deps, env, limit)?)
