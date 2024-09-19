b88ccaa9c87e86aafd8664ee8e4d8d58e87e4e8a
PrismoFinance <moleculardot@proton.me>
2024-09-19 13:24:46 -0400

    Update msg.rs

--- contracts/dca/src/msg.rs
+++ contracts/dca/src/msg.rs
@@ -61,11 +61,11 @@ pub enum ExecuteMsg {
         slippage_tolerance: Option<Decimal>,
         minimum_receive_amount: Option<Uint128>,
         time_interval: Option<TimeInterval>,
-        swap_adjustment_strategy: Option<SwapAdjustmentStrategyParams>,
-        swap_amount: Option<Uint128>,
+        // swap_adjustment_strategy: Option<SwapAdjustmentStrategyParams>,
+        // swap_amount: Option<Uint128>,
     },
     CancelBounty {
-        vault_id: Uint128,
+        bounty_id: Uint128,
     },
     ExecuteTrigger {
         trigger_id: Uint128,
@@ -74,20 +74,20 @@ pub enum ExecuteMsg {
     UpdateConfig {
         executors: Option<Vec<Addr>>,
         fee_collectors: Option<Vec<FeeCollector>>,
-        default_swap_fee_percent: Option<Decimal>,
-        weighted_scale_swap_fee_percent: Option<Decimal>,
+       // default_swap_fee_percent: Option<Decimal>,
+       //  weighted_scale_swap_fee_percent: Option<Decimal>,
         automation_fee_percent: Option<Decimal>,
         default_page_limit: Option<u16>,
         paused: Option<bool>,
         risk_weighted_average_escrow_level: Option<Decimal>,
-        twap_period: Option<u64>,
+        // twap_period: Option<u64>,
         default_slippage_tolerance: Option<Decimal>,
         exchange_contract_address: Option<Addr>,
     },
-    UpdateSwapAdjustment {
-        strategy: SwapAdjustmentStrategy,
-        value: Decimal,
-    },
+   // UpdateSwapAdjustment {
+     //   strategy: SwapAdjustmentStrategy,
+       // value: Decimal,
+   // },
     DisburseEscrow {
         bounty_id: Uint128,
     },
