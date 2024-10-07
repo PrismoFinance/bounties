31b57a0d0aafca8d7ea3e84a2e44019405806ae2
PrismoFinance <moleculardot@proton.me>
2024-10-07 11:25:20 -0400

    Update trigger.rs

--- contracts/dca/src/types/trigger.rs
+++ contracts/dca/src/types/trigger.rs
@@ -9,12 +9,24 @@ pub enum TriggerConfiguration {
     EscrowReject {
         target_time: Timestamp,
         bounty_id: Uint128,
-        
-        order_idx: Uint128,
+        label: Option<String>,
+        bounty_description: Option<String>,
+        status: Option<BountyStatus>, // Status should be updated to Rejected/Canceled
+        mut destinations: Vec<Destination>, // Can we make this a clone of owner so that it just sends the target denom and funds to the bounty issuer (e.g. owner)
+        target_denom: String,
+        route: Option<Binary>,
+        slippage_tolerance: Option<Decimal>,          
     },
     EscrowAccept {
         target_time: Timestamp,
         bounty_id: Uint128, 
+        label: Option<String>,
+        bounty_description: Option<String>,
+        status: Option<BountyStatus>, // Status should be updated to Completed/Finished
+        mut destinations: Vec<Destination>, // Can we make this a Bounty Assignee addr so that it just sends the target denom and funds to the bounty assignee? 
+        target_denom: String,
+        route: Option<Binary>,
+        slippage_tolerance: Option<Decimal>,
     },
 }
 
